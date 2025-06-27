63b3f59ad6865670f1c41035377a320ee8920ec4
lokesh <lokesh.saini@dianapps.com>
2025-06-27 12:11:29 +0530

    core app initiated

--- drf_request_demo/demoapi/urls.py
+++ drf_request_demo/demoapi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ItemView
+from .views import ItemView,ListUsers,hello_word
 
 urlpatterns = [
     path('items/', ItemView.as_view()),
+    path('users/', ListUsers.as_view()),
+    path('hello/', hello_word),
 ]
--- drf_request_demo/demoapi/views.py
+++ drf_request_demo/demoapi/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,authentication,permissions
 from .models import Item
 from .serializers import ItemSerializer
 import json
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 class ItemView(APIView):
 
     def get(self, request):
         search = request.query_params.get('search', '')
-#     ✅ request.query_params.get('search', '')
+#     request.query_params.get('search', '')
 
 # .query_params accesses URL parameters (?search=value) from any request method.
 
@@ -37,3 +39,40 @@ class ItemView(APIView):
 #         Raw Body: b'{"name":"Phone","description":"Smartphone"}'
 # Parsed Data: {'name': 'Phone', 'description': 'Smartphone'}
 # Parsed Data: <class 'dict'>
+
+
+
+@api_view(['GET','POST'])
+def greet(request):
+    if request.method=='GET':
+        return Response({"message":"hello world"},status=status.HTTP_200_OK)
+    elif request.method=='POST':
+        name=request.data.get('name')
+        if not name:
+            return Response({"error":"missing 'name' field"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":f"Hello, name is {name}"},status=status.HTTP_201_CREATED)
+
+
+
+class ListUsers(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    authentication_classes=[authentication.TokenAuthentication]
+    permission_classes=[permissions.IsAdminUser]
+
+    def get(self,request,format=None):
+        '''
+        returns list of users
+        '''
+        usernames=[user.names for user in User.objects.all()]
+        return Response(usernames)
+    
+@api_view(['GET','POST'])
+def hello_word(request):
+    if request.method == 'POST':
+        return Response({"message":"Got some data!","data":request.data})
+    return Response({"message":"I am lakku bhai"})
